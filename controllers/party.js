203d42a148ad42502f9d1ebf5158309c0ea108b5
ngireric123 <ngireric123@gmail.com>
2019-02-04 17:20:30 +0200

    [ft-163695520] add get rout end points

---
+++ controllers/party.js
@@ -0,0 +1,18 @@
+import parties from '../models/party';
+import Joi from 'joi';
+class Party {
+  //Get all political Parties
+  static async getAllParties (req, res){
+    const long = parties.length;
+    if(long  === 0) return res.status(404).send({
+      status: 404,
+      error: 'No political party registered'
+    });
+    res.status(200).send({
+      status: 200,
+      data:parties
+    });
+ }
+}
+
+export default Party;
---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import party from './routes/party';
+
+const app = express ();
+app.use(express.json());
+
+app.use ('/api/v1/parties', party);
+app.get('/', (req, res) => {
+	res.send('Welcome to Politico apps');
+});
+
+const port = process.env.PORT || 8901;
+app.listen(port, () => console.log ( `Listen on port ${port} ...`));
+export default app;
