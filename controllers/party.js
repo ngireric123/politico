521fdbb56070d48d903a87ac7a8275f9960afe0e
ngireric123 <ngireric123@gmail.com>
2019-02-06 16:58:51 +0200

    added controller, packege, routes

--- controllers/party.js
+++ controllers/party.js
@@ -14,38 +14,6 @@ class Party {
     });
  }
 
- // create a new political party
-
-	static async create(req, res){
-
-		const schema = {
-			name: Joi.string().max(255).min(2).required().trim(),
-			hqaddress: Joi.string().max(255).min(2).required().trim(),
-			logourl: Joi.string().max(255).min(3).required().trim()
-		}
-
-		const {error} = Joi.validate(req.body, schema);
-
-		if(error) return res.status(400).send({
-							status: 400,
-							error: error.details[0].message
-						});
-
-		const newParty = {
-			id: parties.length + 1,
-			name: req.body.name,
-			hqaddress: req.body.hqaddress,
-			logourl: req.body.logourl
-		}
-
-		parties.push(newParty);
-
-		res.status(201).send({
-			status: 201,
-			data: newParty
-		});
-	}
-
 
 
 
--- routes/party.js
+++ routes/party.js
@@ -3,8 +3,9 @@ import Party from '../controllers/party';
 
 const router = express.Router();
 
-//router.post('/', Party.create);
+
 router.get('/', Party.getAllParties);
+router.post('/', Party.create);
 //router.get('/:id', Party.getOne);
 //router.patch('/:id', Party.update);
 //router.delete('/:id', Party.remove);
