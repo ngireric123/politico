d25df3b88399633c9435fd4c7c0ea30ad1bd9b27
ngireric123 <ngireric123@gmail.com>
2019-02-07 15:38:56 +0200

    [ft-163763825] add get all political office

--- controllers/office.js
+++ controllers/office.js
@@ -32,6 +32,44 @@ class Office {
 		});
 
 	}
+
+  //get all political offices
+
+	static async getAll(req, res){
+		const count = offices.length;
+		if(count === 0)	return res.status(404).send({
+									status: 404,
+									error: 'No political office found'
+								});
+
+		res.status(200).send({
+			status: 200,
+			data: offices
+		});
+	}
+
+
+  	// get specific political office
+
+  	static async getOne(req, res){
+  		const office_id = parseInt(req.params.id);
+  		const result = [];
+  		for(let i = 0; i < offices.length; i ++){
+  			if(offices[i].id == office_id){
+  				result.push(offices[i]);
+  			}
+  		}
+  	if(result.length == 0) return res.status(404).send({
+  										status: 404,
+  										error: "political office not found"
+  									});
+  	res.status(200).send({
+  			status: 200,
+  			data: result
+  		})
+  	}
+
+
 }
 
 export default Office;
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import party from './routes/party';
+import office from './routes/office';
 
 const app = express ();
 app.use(express.json());
 
 app.use ('/api/v1/parties', party);
+app.use('/api/v1/offices', office);
+
 app.get('/', (req, res) => {
 	res.send('Welcome to Politico apps');
 });
