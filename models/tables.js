10348bc2b2ef9b17dd2f28b8a06f403405eb2d91
ngireric123 <ngireric123@gmail.com>
2019-02-19 14:07:31 +0200

    [ch-164074492] creating table

---
+++ models/tables.js
@@ -0,0 +1,100 @@
+import pool from './db';
+
+
+const create = () => {
+  const usersTable = `CREATE TABLE IF NOT EXISTS
+     users(
+       id SERIAL PRIMARY KEY,"firstName" VARCHAR(24) NOT NULL,"lastName" VARCHAR(10) NOT NULL,
+       "otherName" VARCHAR(10) NULL,
+       "email" VARCHAR(50) NOT NULL,
+       "phoneNumber" VARCHAR(10) NOT NULL,
+       password TEXT NOT NULL,
+       "passportUrl" VARCHAR(20) NOT NULL,
+       "isAdmin" BOOLEAN NOT NULL DEFAULT false
+     )`;
+
+  const partiesTable = `CREATE TABLE IF NOT EXISTS
+    party(
+     id SERIAL PRIMARY KEY,
+     name VARCHAR(50) NOT NULL,
+     hqaddress VARCHAR(50) NOT NULL,
+     logourl VARCHAR(50) NOT NULL
+    )`;
+
+
+  const candidatesTable = `CREATE TABLE IF NOT EXISTS
+   candidate(
+     id SERIAL,
+     office INT NOT NULL REFERENCES office(id) ON DELETE CASCADE ON UPDATE CASCADE,
+     party INT NOT NULL REFERENCES party(id) ON DELETE CASCADE ON UPDATE CASCADE,
+     candidate INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+     votes INT NOT NULL DEFAULT 0,
+     PRIMARY KEY (id, office, candidate)
+   )`;
+
+  const votesTable = `CREATE TABLE IF NOT EXISTS
+  vote(
+     id SERIAL PRIMARY KEY,
+     createdon TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     createdby INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+     office INT NOT NULL REFERENCES office(id) ON DELETE CASCADE ON UPDATE CASCADE,
+     candidate INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+  )`;
+
+  const petitionsTable = `CREATE TABLE IF NOT EXISTS
+  petition(
+     id SERIAL PRIMARY KEY,
+     createdon TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     createdby INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+     office INT NOT NULL REFERENCES office(id) ON DELETE CASCADE ON UPDATE CASCADE,
+     body TEXT NOT NULL
+  )`;
+  const officesTable = `CREATE TABLE IF NOT EXISTS
+  office(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   type VARCHAR(50) NOT NULL
+  )`;
+  const newUserTable = `INSERT INTO 
+  usersTable(
+    "firstName",
+    "lastName",
+    "otherName",
+    email,
+    "phoneNumber",
+    password,
+    "passportUrl",
+    "isAdmin"
+    ) VALUES (
+    'ngirababyeyi',
+    'eric',
+    ' ',
+    'ngireric123@gmail.com',
+    '0788716711',
+    '123',
+    'my_pic.jpg',
+    true
+    )`;
+
+  const queries = `${usersTable};${partiesTable}; ${candidatesTable}; ${votesTable}; ${petitionsTable};${officesTable}; ${newUserTable}`;
+
+  pool.query(queries);
+};
+
+const deleteTables = () => {
+  const users = 'DROP TABLE IF EXISTS users CASCADE';
+  const petitions = 'DROP TABLE IF EXISTS petition';
+  const candidates = 'DROP TABLE IF EXISTS candidate';
+  const offices = 'DROP TABLE IF EXISTS office CASCADE';
+  const votes = 'DROP TABLE IF EXISTS vote';
+  const parties = 'DROP TABLE IF EXISTS party CASCADE';
+  const deleteQueries = `${users};${petitions}; ${candidates}; ${offices}; ${votes}; ${parties}`;
+  pool.query(deleteQueries);
+};
+
+module.exports = {
+  create,
+  deleteTables,
+};
+
+require('make-runnable');
