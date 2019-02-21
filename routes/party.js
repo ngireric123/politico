a764945c28b4ddaaf13d82e4f2284863cbab1ef3
ngireric123 <ngireric123@gmail.com>
2019-02-21 11:17:14 +0200

    [ft-163695572] Edit a specific political party

--- routes/party.js
+++ routes/party.js
@@ -2,6 +2,9 @@ import express from 'express';
 import Party from '../controllers/party';
 
 const router = express.Router();
+router.post('/', Party.createParty);
 router.get('/', Party.getParties);
-
+router.get('/:id', Party.getParty);
+router.patch('/:id', Party.updateParty);
+router.delete('/:id', Party.delete);
 export default router;
